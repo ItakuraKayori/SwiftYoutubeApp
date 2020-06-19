f5f1e408c430e1b5f2e798bdf593c65a9be2515f
ItakuraKayori <mamedaifuku2525@gmail.com>
2020-06-19 16:16:09 +0900

    Initial Commit

---
+++ SwiftYoutubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftYoutubeApp
+//
+//  Created by 板倉果保 on 2020/06/19.
+//  Copyright © 2020 Itakura Kayori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
